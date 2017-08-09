9d67464b8f0e0370b32e4f3c48669c10fb2584c9
lijianbo <nighthary@163.com>
2017-08-09 16:18:33 +0800

    update demo code

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # nodejs-server-demo
 this is a nodejs server demo project
+
+###	1.需要安装nodejs
+
+
+
+```Js
+#install nodejs package modules
+npm install
+
+#run
+npm run dev				
+
+```
+
+### 2. 自定义路径
+
+> 自定义访问路径请修改server.js中的path_url即可
+
+### 3. 其他
+
+>每次修改路径都需要重新启动nodejs
--- server.js
+++ server.js
@@ -1,40 +1,18 @@
-var express = require('express');
-var app = express();
-var path = require('path');
-var httpProxy = require('http-proxy');
+const express = require('express');
+const app = express();
+const path = require('path');
+const httpProxy = require('http-proxy');
 
-var opn = require('opn')
+const opn = require('opn')
 
-let open_port = 8003,
-	path_url = 'index.html',
-	open_url = `http://localhost:${open_port}`;
-
-var proxy = new httpProxy.createProxyServer({
-	target: {
-		host: 'kdkc365.com',
-		port: 80
-	}
-});
-
-
-//代理中间件
-app.use(function(req, res, next) {
-	console.log(req.url);
-	if (req.url.match(new RegExp('^(\/main\/|\/shoppingcart\/|\/cart\/|\/member\/|\/my\/|\/maintenance\/)'))) {
-		console.log('proxy to: kdkc365.com');
-		proxy.web(req, res);
-	} else if (req.url.match(new RegExp('^(\/changanshop_pay\/)'))) {
-		console.log('proxy to: kdkc365.com');
-		newProxy.web(req, res);
-	} else {
-		next();
-	}
-});
+let open_port = 8004,
+	path_url = '/GS-UI/index.html',
+	open_url = `http://localhost:${open_port}${path_url}`;
 
 app.use(express.static(path.resolve('.')));
 
 app.get('/', function(req, res) {
-	res.redirect(path_url);
+	res.redirect('./GS-UI/index.html');
 });
 
 
